d3c6df1084a6eb620256628ef4b03337262e09de
kevin79000 <guellec.coachingpro@gmail.com>
2026-05-19 19:07:36 +0200

    Add Metal Mania gothic font on RC logo, titles, main buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'repcore-v13';
+const CACHE = 'repcore-v14';
 const FILES = ['./index.html', './manifest.json', './icons/icon-192x192.png', './icons/icon-512x512.png'];
 
 self.addEventListener('install', e => {
