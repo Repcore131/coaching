b95b8224128b3d20e9797d32c803d0698f3552b4
Repcore131 <guellec.coachingpro@gmail.com>
2026-09-09 19:19:31 +0200

    Add files via upload

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'repcore-v1082';
+const CACHE = 'repcore-v1083';
 // LA SEULE VERSION QUI NE REPORTE PAS LES ILLUSTRATIONS.
 // Le report d'un cache a l'autre traite /exercices/ en PRIORITAIRE : c'est
 // ce qui evite de retelecharger 3,3 Mo a chaque deploiement. Mais le
